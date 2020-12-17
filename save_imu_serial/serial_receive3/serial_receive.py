1e611c9835983dcb1f2be3a5bece6a4e1c356991
dmasatoumae <masatodomae@gmail.com>
2020-12-17 17:52:01 +0900

    fix

--- save_imu_serial/serial_receive3/serial_receive.py
+++ save_imu_serial/serial_receive3/serial_receive.py
@@ -3,9 +3,12 @@ import csv
 import keyboard
 
 ser = serial.Serial('/dev/ttyACM2', 9600, timeout=None)
-with open('save.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile, lineterminator='\n')
-
+print('input file name')
+file_name = input()
+with open(file_name, 'a') as csvfile:
+    #writer = csv.writer(csvfile, lineterminator='\n')
+    csvfile.write('save_start' + "\n")
+    #writer.writerow('save start')
     while True:
         line = ser.readline()
         print(line)
@@ -13,7 +16,8 @@ with open('save.csv', 'w') as csvfile:
             if keyboard.is_pressed('s'):
                 print('key pressed')
                 #x = float(line.rstrip().decode(encoding='utf-8'))
-                writer.writerow(line.rstrip().decode('utf-8'))
+                csvfile.write(line.rstrip().decode('utf-8') + "\n")
+                #writer.writerow(line.rstrip().decode('utf-8'))
                 pass
             else:
                 pass
